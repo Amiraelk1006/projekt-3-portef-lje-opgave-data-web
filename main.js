f9632b07d1e1d67380a7d0be7d80fdcaf2e2fd5e
Lenovo <Amiraelk1006@gmail.com>
2025-10-07 10:56:23 +0200

    Commit Message

---
+++ main.js
@@ -0,0 +1,30 @@
+
+//02 - Startup name generator
+
+
+
+// First I make two lists of words for the startup names
+const firstPart = ["Tech", "Inno", "Smart", "Blue", "Next", "Bright", "Quantum", "Rapid", "Future", "Digital"];
+const secondPart = ["Vision", "Solutions", "Systems", "Works", "Dynamics", "Hub", "Network", "Innovations", "Labs", "Path"];
+
+// Now I want to make a function that picks a random word from each list
+function getStartupName() {
+    // Pick a random index for the first word
+    const firstIndex = Math.floor(Math.random() * firstPart.length);
+
+    // Pick a random index for the second word
+    const secondIndex = Math.floor(Math.random() * secondPart.length);
+
+    // Combine the two words with a space in between and return it
+    return firstPart[firstIndex] + " " + secondPart[secondIndex];
+}
+
+// Then i test it a few times to see some names
+console.log("Your startup name could be: " + getStartupName());
+console.log("Your startup name could be: " + getStartupName());
+console.log("Your startup name could be: " + getStartupName());
+
+
+
+
+//03 - Treasure Hunter
