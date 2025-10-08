e4b753a554a419caa8817ef4d8d9d925a76c361b
Lenovo <Amiraelk1006@gmail.com>
2025-10-08 11:06:20 +0200

    Commit Message

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 
 //02 - Startup name generator
 
-
 // Array 1: First part of the startup name
 const firstword = ["Tech", "Inno", "Smart", "Blue", "Next", "Bright", "Quantum", "Rapid", "Future", "Digital"];
 // Array 2: Second part of the startup name
@@ -9,11 +8,15 @@ const secondword = ["Vision", "Solutions", "Systems", "Works", "Dynamics", "Hub"
 
 //Her laver jeg et navn
 const getStartUpName = () =>{
+
+    //her vælger et tilfældigt tal/navn fra begge arrays
     const firstNum = Math.floor(Math.random() * firstword.length)
     const secondNum = Math.floor(Math.random() * secondword.length)
+
+    // her sammensætter jeg de to arrays og retunere det
     return `${firstword[firstNum]} ${secondword[secondNum]}`
 }
-
+//her logger jeg ud
 console.log("Your startup name is: " + getStartUpName() )
 
 
